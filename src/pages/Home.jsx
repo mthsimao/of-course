b263d556d0b1a2c31098bee4514eb079d0eb3bec
Matheus Simão <98268509+mthsimao@users.noreply.github.com>
2025-11-19 12:19:32 -0300

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen w-full">
       <Navbar />
-      <div className="flex flex-col flex-1">
+      <div className="bg-section-bg flex flex-col gap-6 flex-1">
         <Hero />
         <Features />
         <Courses />
